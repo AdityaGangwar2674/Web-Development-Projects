bcfc1c715de6a396f928a34fe3b0b38655c8baf4
AdityaGangwar2674 <124704754+AdityaGangwar2674@users.noreply.github.com>
2023-09-17 14:47:42 +0530

    Add files via upload

---
+++ Bubble Game/bubble.js
@@ -0,0 +1,48 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+function increaseScore() {
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+function getNewHit() {
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+function makeBubble() {
+    var clutter = "";
+    for (var i = 1; i <= 200; i++) {
+        var rn = Math.floor(Math.random() * 10);
+        clutter += `<div class="bubble">${rn}</div>`;
+    }
+
+    document.querySelector("#pbtm").innerHTML = clutter
+}
+function runTimer() {
+    var timerint = setInterval(function () {
+        if (timer > 0) {
+            timer = timer - 1;
+            document.querySelector("#timerval").textContent = timer;
+        }
+        else {
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = ` <h1
+            style="color: red;display: flex;align-items: center;justify-content: center;font-size: 200px;font-weight: bolder;position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%);">Game Over</h1>`;
+        }
+    }, 1000);
+
+}
+
+document.querySelector("#pbtm").addEventListener("click", function (dets) {
+    var clickedNumber = Number(dets.target.textContent);
+    if (clickedNumber == hitrn) {
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+})
+
+runTimer();
+makeBubble();
+getNewHit();
